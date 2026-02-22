4b38ba21ad752eb03122d8239a59d7d06d0eeb54
ZoyaRasheed <zoyaa4840@gmail.com>
2026-02-23 04:14:09 +0500

    feat: restructure project with express setup, add mongoose and cors dependencies, and implement error handling

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+
+
+//app.js is entirely for express configuration and index.js would be the entry point of application
+const app = express()
+
+// Basic configuration
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))// this is public viewable
+
+
+//CORS configurations
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173" ,
+    credentials : true,
+    methods : ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
+    allowedHeaders:["Content-Type","Authorization"]
+}))
+
+export default app
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import app from './app.js'
+import connectDB from './db/index.js'
+
+const PORT = process.env.PORT
+
+connectDB()
+.then(()=>{
+    app.listen(PORT,()=>{
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
+})
+.catch(error=>{
+    console.error("MongoDB connection error",error)
+    process.exit(1)
+})
+
